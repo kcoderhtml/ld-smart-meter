808a7eeda239e2d946a696e47a91f87b1f96141c
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-06-14 17:01:46 -0400

    :recycle: {Broken} Format Time field with quotes and include SCM field in an object

--- format.sh
+++ format.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # The input file
-read -p "Enter the file name: " file
+# read -p "Enter the file name: " file
+file="test.json"
 
 # Loop over the objects and create an array
 array=()
@@ -14,13 +15,24 @@ for (( i=0; i<${#array[@]}-1; i++ )); do
   array[$i]="${array[$i]},"
 done
 
-# Enclose the Time field in quotes
+# Enclose the Time field in quotes and format the SCM field
 for (( i=0; i<${#array[@]}; i++ )); do
-  array[$i]=$(echo "${array[$i]}" | sed 's/Time:\(.*\)/"Time":"\1"/;s/ SCM:/","SCM":"/')
+  array[$i]=$(echo "${array[$i]}" | sed 's/Time:\(.*\) SCM:/{ "Time":"\1", "SCM":/')
+  array[$i]=$(echo "${array[$i]}" | sed 's/ ID:/{ "ID":"/')
+  array[$i]=$(echo "${array[$i]}" | sed 's/ Type:/", "Type":"/')
+  array[$i]=$(echo "${array[$i]}" | sed 's/ Tamper:/{ "Tamper":/')
+  array[$i]=$(echo "${array[$i]}" | sed 's/ Phy:/{ "Phy":"/')
+  array[$i]=$(echo "${array[$i]}" | sed 's/ Enc:/", "Enc":"/')
+  array[$i]=$(echo "${array[$i]}" | sed 's/ Consumption:/"}, "Consumption":/')
+  array[$i]=$(echo "${array[$i]}" | sed 's/ CRC:/, "CRC":"/')
+  array[$i]=$(echo "${array[$i]}" | sed 's/$/"} }/')
 done
 
 # Add square brackets to the start and end of the array
 array=( "[" "${array[@]/%/ }" "]" )
 
+# Save data to a file
+echo "${array[@]}" > output.json
+
 # Print the formatted array
-echo "${array[@]}" | jq .
+jq . output.json
