aa6d520553f10a1d82ceafe2c98fd2f0d04ec105
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-06-14 20:53:11 -0400

    :hammer: Fix sed expressions in format.sh script

--- format.sh
+++ format.sh
@@ -11,7 +11,7 @@ fi
 # Loop over the objects and create an array
 array=()
 while read -r line; do
-    line=$(echo $line | sed 's/{Time:\([^ ]*\)/{"Time":"\1",/' | sed 's/ SCM:{ID:\([^ ]*\)/ "SCM":{"ID":"\1",/' | sed 's/ Type:\([^ ]*\)/ "Type":"\1",/' | sed 's/ Tamper:{Phy:\([^ ]*\)/ "Tamper":{"Phy":"\1",/' | sed 's/ Enc:\([^ ]*\)/ "Enc":"\1"},/' | sed 's/ Consumption: \([^ ]*\)/ "Consumption":"\1",/' | sed 's/ CRC:\([^}]*\)/ "CRC":"\1"/')
+    line=$(echo $line | sed 's/{Time:\([^ ]*\)/{"Time":"\1",/' | sed 's/ SCM:{ID:\([^ ]*\)/ "SCM":{"ID":"\1",/' | sed 's/ Type:\([^ ]*\)/ "Type":"\1",/' | sed 's/ Tamper:{Phy:\([^ ]*\)/ "Tamper":{"Phy":"\1",/' | sed 's/ Enc:\([^}]*\)}/ "Enc":"\1"},/' | sed 's/ Consumption: \([^ ]*\)/ "Consumption":"\1",/' | sed 's/ CRC:\([^}]*\)/ "CRC":"\1"/')
     array+=("$line")
 done <"$file"
 
@@ -25,15 +25,24 @@ array=("[" "${array[@]/%/ }" "]")
 
 # Run the array through jq and write the output to a temporary file
 tmpfile=$(mktemp)
-printf "%s\n" "${array[@]}" | jq . > "$tmpfile"
+printf "%s\n" "${array[@]}" | jq . > "$tmpfile" 2>&1
 
 # Check if jq was successful
 if [ $? -eq 0 ]; then
     # Move the temporary file to the output file
     mv "$tmpfile" "$file"
 else
-    # Print an error message and delete the temporary file
+    # Print an error message and the line that caused the error
     echo "-------------------------"
-    echo "jq failed to process the input file"
+    cat "$tmpfile"
+    line_num=$(cat "$tmpfile" | grep -oE 'line [0-9]+' | grep -oE '[0-9]+')
+
+    line=$(printf "%s\n" "${array[@]}" | sed "${line_num}q;d")
+    unfilteredline=$(cat $file | sed "${line_num}q;d")
+
+    echo "Error Line: $line"
+    echo "-------------------------"
+    echo "Original file Error Line: $unfilteredline"
+    printf "%s\n" "${array[@]}" > test.json
     rm "$tmpfile"
 fi
